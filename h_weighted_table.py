fixed incorrect h weighting, parlalelised

h_weighted_table.py
@@ -28,6 +28,8 @@ def generate_h_weighted_table(nodustmode,highdensemode,table_date,mass=0.0001):
  
     suffixes = dust_suffix+dense_suffix
 
+#     print("H_WEIGHTED_TABLE PROCESS STARTED - ",nodustmode,highdensemode,table_date,mass,suffixes)
+
     #table_date = "17117"
     #table_date = "291117"
 
@@ -96,15 +98,13 @@ def generate_h_weighted_table(nodustmode,highdensemode,table_date,mass=0.0001):
         masses = mass
     else:
         masses = [mass]
-
-    #for mass_p in np.arange(1,11)*0.01:
-    for mass_p in masses:
-    #for mass_p in [0.02,0.04,0.06,0.08,0.1]:
+    
+    def integrate_and_dump_table(mass_p):
         print("mass=",mass_p)
 
-        # h=nH**(-1./3.)/h_dense_factor
+        # h=nH**(-1./3.)*h_dense_factor
         # h comes out in pc
-        h_dense_factor = 0.3404 * (mass_p/0.1)**(-1/.3)
+        h_dense_factor = (mass_p/0.1)**(1./3.)/0.3404
 
         # conversion factor from solar masses/pc**2 into N_H in cm**-2
         molecular_mass = 4./(1.+3.*.76)
@@ -160,17 +160,25 @@ def generate_h_weighted_table(nodustmode,highdensemode,table_date,mass=0.0001):
         #         if np.sum(~index_is_max)>0:
         #             vals[~index_is_max] = agn_column_in_vals[tau_indices[~index_is_max]]*(1.-tau_weights[~index_is_max]) + agn_column_in_vals[tau_indices[~index_is_max]+1]*tau_weights[~index_is_max]
                 vals = surf0_p
-                print(vals.shape)
+#                 print(vals.shape)
             else:
                 vals = get_struct_attribute(tabStructs,attribute)
-                print(vals.shape)
+#                 print(vals.shape)
             vals = vals * tab_dm
             vals = np.sum(vals,1)
             attrib_out.append(vals)
     
         attrib_out = np.array(attrib_out)
+        outfile = "shrunk_table_"+time.strftime("%d%m%y")+"_m{}_hsmooth_tau".format(mass_p)+suffixes+".dat"
+        print("Dumping table:",outfile)
+        np.savetxt(outfile,attrib_out.T)
+    #for mass_p in np.arange(1,11)*0.01:
+    for mass_p in masses:
+        integrate_and_dump_table(mass_p)
+#     with Pool(processes=64) as pool:
+#         pool.map(integrate_and_dump_table,masses)
+    #for mass_p in [0.02,0.04,0.06,0.08,0.1]:
 
-        np.savetxt("shrunk_table_"+time.strftime("%d%m%y")+"_m{}_hsmooth_tau".format(mass_p)+suffixes+".dat",attrib_out.T)
 
 
 if __name__ == '__main__':

plot_h_weighted.py
@@ -4,21 +4,37 @@ from sys import path,exit
 path.append("../src/")
 import tab_interp
 
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as P
 
 print("Loading tables")
 
-mass_p = np.arange(1,11)*0.01
-#mass_p = np.array([0.01,0.02])
+# mass_p =[1.e-5,5.e-5,1.e-4,5.e-4,1.e-3,5.e-3,1.e-2]
+# mass_p =[1.e-5,1.e-4,1.e-3,1.e-2,3.e-2,1.e-1,3.e-1,1.,10.]
+# mass_p =[1.e-5,1.e-4,1.e-3,1.e-2,3.e-2,1.e-1,3.e-1,1.,10.]
+# mass_p = np.linspace(0.01,1.,20)
+# mass_p = [0.01,0.05,0.1]
 
-# mass_p = [0.02,0.04,0.08,0.1]
+mass_p =[1.e-5,5.e-5,1.e-4,5.e-4,1.e-3,5.e-3,1.e-2]
+
 
 # tau_tables = [tab_interp.CoolHeatTab("shrunk_table_labels_171117tau.dat","shrunk_table_281117_m{}_hsmooth_tau.dat".format(x)) for x in mass_p]
-tau_tables = [tab_interp.CoolHeatTab( ("shrunk_table_labels_291117tau.dat"),
-                                            ("shrunk_table_291117_m{}_hsmooth_tau.dat".format(x)),
-                                            ("shrunk_table_labels_011217taunodust.dat"),
-                                            ("shrunk_table_011217_m0.1_hsmooth_taunodust.dat")
-                                            ) for x in mass_p]
+# tau_tables = [tab_interp.CoolHeatTab( ("shrunk_table_labels_291117tau.dat"),
+#                                             ("shrunk_table_291117_m{}_hsmooth_tau.dat".format(x)),
+#                                             ("shrunk_table_labels_011217taunodust.dat"),
+#                                             ("shrunk_table_011217_m0.1_hsmooth_taunodust.dat")
+#                                             ) for x in mass_p]
+
+date = "140818"
+
+tau_tables = [tab_interp.CoolHeatTab( ("shrunk_table_labels_{}tau.dat".format(date)),
+                                        ("shrunk_table_{}_m{}_hsmooth_tau.dat".format(date,x)),
+                                        ("shrunk_table_labels_{}taunodust.dat".format(date)),
+                                        ("shrunk_table_{}_m{}_hsmooth_taunodust.dat".format(date,x)),
+                                        ("shrunk_table_labels_{}taudense.dat".format(date)),
+                                        ("shrunk_table_{}_m{}_hsmooth_taudense.dat".format(date,x))
+                                        ) for x in mass_p]
 
 table_attributes = ['dHeat','dCool','dustT','arad','dg','opac_abs','opac_scat','column_out']
 
@@ -26,6 +42,9 @@ nH_p = 10**6.
 TK_p = 10.**2.5
 flux_p = 10.**6.
 tau_p = 0.
+
+def m_to_h(m):
+    return nH_p**(-1./3.)/0.3404*(m/0.1)**(1./3.)
 
 print("Interpolating from tables")
 
@@ -37,11 +56,24 @@ vals = np.array(vals)
 
 vals = np.vstack((vals.T,mass_p)).T
 
-#np.savetxt("h_weighted_test.txt",vals)
+# np.savetxt("../data/h_weighted_test.txt",vals)
 
-P.figure()
-P.scatter(mass_p,vals[:,3])
-P.xlabel(r"$M_p$ (M$_\odot$)")
-P.ylabel(r"$\log a_r$ (cm s$^{-2}$)")
+fig,ax1=P.subplots()
+ax1.scatter(np.log10(mass_p),vals[:,3])
+# ax1.scatter(mass_p,vals[:,3])
+ax1.set_xlabel(r"$\log M_p$ (M$_\odot$)")
+ax1.set_ylabel(r"$\log a_r$ (cm s$^{-2}$)")
+
+# ax2 = ax1.twiny()
+# ax2.scatter(np.log10(mass_p),vals[:,3])
+# # P.tight_layout()
+# masslocs = ax2.get_xticks()
+# m = 10.**masslocs
+# h_p = m_to_h(m)
+# ax2.set_xticklabels(["%.1f"%x for x in np.log10(h_p)])
+# ax2.set_xlabel(r"$\log h_p$ (pc)")
+
+# P.yscale('log')
+# P.xscale('log')
 P.savefig("../../figures/h_weighted_test.pdf")
 P.close('all')

process_table_tau.py
@@ -61,6 +61,7 @@ def process_table_tau(taumode,nodustmode,highdensemode,tableFile,taumax=5.):
     dense_suffix = "dense" if highdensemode else ""
  
     suffixes = tau_suffix+dust_suffix+dense_suffix
+    print("Saving labels")
     f = open("shrunk_table_labels_"+time.strftime("%d%m%y")+suffixes+".dat",'w')
 
     for ar in [denses,temps,intensities,column_in]:
@@ -92,7 +93,7 @@ def process_table_tau(taumode,nodustmode,highdensemode,tableFile,taumax=5.):
 
     for id in range(nd):
         for it in range(nt):
-            print(id,it)
+#             print(id,it)
             for ii in range(ni):
                 index0 = d_offset[id]+i_offset[ii]+t_offset[it]
                 index1 = index0+nc
@@ -106,7 +107,7 @@ def process_table_tau(taumode,nodustmode,highdensemode,tableFile,taumax=5.):
 
     for id in range(nd):
         for it in range(nt):
-            print(id,it)
+#             print(id,it)
             for ii in range(ni):
                 outdata = []
                 for iout,outp in enumerate(alloutp):
@@ -118,6 +119,7 @@ def process_table_tau(taumode,nodustmode,highdensemode,tableFile,taumax=5.):
                 alloutdata=np.hstack([alloutdata,outdata])
 
     alloutdata = np.array(alloutdata)
+    print("Saving table")
     np.savetxt("shrunk_table_"+time.strftime("%d%m%y")+suffixes+".dat",alloutdata.T)
 
 if __name__ == '__main__':

produce_tables_from_text_complete.py
@@ -1,13 +1,42 @@
 import process_table_tau
 import h_weighted_table
 import time
+from multiprocessing import Pool
+from functools import partial
+import sys
 
-for nodustmode,highdensemode,filename in [ (False,True,"highden_260118.txt"),
-                                           (False,False,"tables_070818.txt"),
-                                           (True,False,"nodust_301117.txt")
-                                            ]:
-    for taumode in [True,False]:
-        process_table_tau.process_table_tau(taumode,nodustmode,highdensemode,filename,taumax=7.)
-    table_date = time.strftime("%d%m%y")
-#     print(table_date)
-    h_weighted_table.generate_h_weighted_table(nodustmode,highdensemode,table_date,mass=0.0001)
+import numpy as np
+
+masses = [1.e-5,1.e-4,1.e-3,1.e-2,3.e-2,1.e-1,3.e-1,1.,10.]
+
+# masses = np.linspace(0.01,1.,20)
+# masses = [0.01,0.05,0.1]
+
+# masses = [1.e-4]
+
+table_parameters = [ [False,True,"highden_260118.txt"],
+                     [False,False,"tables_100818.txt"],
+                     [True,False,"nodust_301117.txt"]
+                                            ]
+
+table_parameters_taumode = [ [True]+line for line in table_parameters ] + [ [False]+line for line in table_parameters ]
+
+
+table_parameters_mass = []
+for mass in masses:
+    table_parameters_mass = table_parameters_mass + [line+[mass] for line in table_parameters]
+
+table_date = time.strftime("%d%m%y")
+for line in table_parameters_mass:
+    line[2] = table_date
+
+def process_table_tab_for_pool(prams):
+    process_table_tau.process_table_tau(*prams,taumax=7.)
+
+def generate_h_weighted_table_for_pool(prams):
+    h_weighted_table.generate_h_weighted_table(*prams)
+
+with Pool(processes=64) as pool:
+#     pool.map(process_table_tab_for_pool,table_parameters_taumode)
+    pool.map(generate_h_weighted_table_for_pool,table_parameters_mass)
+